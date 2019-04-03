4f40b414487657e5996d8446041b80c96d6bde8a
adamos486 <adamos486@gmail.com>
2019-04-03 15:53:58 -0700

    First checkin

---
+++ train_simple_nn.py
@@ -0,0 +1,92 @@
+import matplotlib
+matplotlib.use("Agg")
+
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import SGD
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+import pickle
+import cv2
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True, help="path to input dataset of images")
+ap.add_argument("-m", "--model", required=True, help="path to output trained model")
+ap.add_argument("-l", "--label-bin", required=True, help="path to output label binarizer")
+ap.add_argument("-p", "--plot", required=True, help="path to output accuracy/loss plot")
+args = vars(ap.parse_args())
+
+print("[INFO] loading images...")
+data = []
+labels = []
+
+imagePaths = sorted(list(paths.list_images(args["dataset"])))
+random.seed(42)
+random.shuffle(imagePaths)
+
+for imagePath in imagePaths:
+  image = cv2.imread(imagePath)
+  image = cv2.resize(image, (32, 32)).flatten()
+  data.append(image)
+
+  label = imagePath.split(os.path.sep)[-2]
+  labels.append(label)
+
+data = np.array(data, dtype="float")
+labels = np.array(labels)
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=42)
+
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+[1, 0, 0] # cats
+[0, 1, 0] # dogs
+[0, 0, 1] # pandas
+
+model = Sequential()
+model.add(Dense(1024, input_shape=(3072,), activation="sigmoid"))
+model.add(Dense(512, activation="sigmoid"))
+model.add(Dense(len(lb.classes_), activation="softmax"))
+
+INIT_LR = 0.01
+EPOCHS = 75
+
+print("[INFO] training network...")
+opt = SGD(lr=INIT_LR)
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+H = model.fit(trainX, trainY, validation_data=(testX, testY),
+              epochs=EPOCHS, batch_size=32)
+
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1),
+                            target_names=lb.classes_))
+
+N = np.arange(0, EPOCHS)
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, H.history["loss"], label="train_loss")
+plt.plot(N, H.history["val_loss"], label="val_loss")
+plt.plot(N, H.history["acc"], label="train_acc")
+plt.plot(N, H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy (Simple NN)")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.savefig(args["plot"])
+
+print("[INFO] serializing network and label binarizer...")
+model.save(args["model"])
+f = open(args["label_bin"], "wb")
+f.write(pickle.dumps(lb))
+f.close()
